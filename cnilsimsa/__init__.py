c048d3a4c332b5dcc06053467a92a3ef13d2c79c
rw-r-r-0644 <rw-r-r-0644@protonmail.com>
2025-06-19 19:27:04 +0200

    __init__.py: fix pkg_resources deprecation warning

--- cnilsimsa/__init__.py
+++ cnilsimsa/__init__.py
@@ -1,13 +1,8 @@
-import pkg_resources
+from importlib.resources import files
 import sys
 
 from _nilsimsa import compare_hexdigests
 
 
-version_file = pkg_resources.resource_filename(__name__, 'VERSION')
-with open(version_file) as vf:
-    __version__ = vf.read()
-del version_file
-
-
+__version__ = files(__package__).joinpath('VERSION').read_text()
 __all__ = ['compare_hexdigests', '__version__']
